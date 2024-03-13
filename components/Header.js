1f2ef3047d268683bdbcbfd6f24f4dba6e492df5
Elysium Hosack <elysium.hosack@mymail.champlain.edu>
2024-03-13 00:21:42 -0400

    added nav

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,30 @@
 export default function Header({ title }) {
-  return <h1 className="title">{title}</h1>
+  return (
+    <nav>
+      <ul>
+        <li className="nav-item logo">
+          <img src="/logo.png" alt="Burlington Electronics Repair logo" />
+        </li>
+        <li className="nav-item">
+          <a href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="/services">Services</a>
+        </li>
+        <li className="nav-item">
+          <a href="/contact">Contact</a>
+        </li>
+
+        <li>
+          <a href="/about">About</a>
+        </li>
+
+        <li>
+          <a href="https://venmo.com/u/repairbtn">Pay</a>
+        </li>
+
+      </ul>
+    </nav>
+
+  );
 }
--- pages/services.js
+++ pages/services.js
@@ -2,16 +2,18 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-export default function Home() {
+export default function Services() {
   return (
     <div className="container">
       <Head>
         <title>Burlington Electronics Repair</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      
 
       <main>
         <Header />
+        <h1>Services</h1>
         <p className="description">
           Give your electronics a second chance -- don't toss them, fix them!
         </p>
